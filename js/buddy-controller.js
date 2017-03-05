93b035bea23c5c1465263d8840a34d2f2eb98e84
Arash Shoar <b_arash_s@yahoo.com>
2017-03-04 22:37:35 -0800

    The filters work for status completely

--- js/buddy-controller.js
+++ js/buddy-controller.js
@@ -8,11 +8,51 @@ angular.module('arashTcsApp')
 
 
 	vm = this;
+	// vm.searchModel.status = "Busy";
+	$http.get('data/buddy.js')
+	.then(function(response) {
+		vm.persons = response.data;
+	}, function(reason) {
 
-	vm.testFunction = function function_name (argument) {
-		
-		console.log('SalamName');
-	
+	});
+
+	vm.statusForFilter = ['Busy', 'Idle', 'Available', 'Offline'];
+	//vm.statusForFilter = [];
+
+	vm.setStatusForFilter = function(event, value) {
+
+
+
+
+        var elem = event.target;
+
+console.log('elem', elem.checked);
+
+        if (elem.checked) {
+
+        	if (vm.statusForFilter.includes(value)) {
+        		return;
+        	}
+        	else {
+        		vm.statusForFilter.push(value);
+        	}
+
+        }
+        else {
+
+        	if (!vm.statusForFilter.includes(value)) {
+        		return;
+        	}
+        	else {
+        		var index = vm.statusForFilter.indexOf(value);
+        		vm.statusForFilter.splice(index, 1);
+        	}
+        }
+	};
+
+
+	vm.filterByStatus = function(status) {
+		return vm.statusForFilter.includes(status);
 	};
 
 	
